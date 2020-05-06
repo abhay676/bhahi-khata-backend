4cff28f5b1ba401ef0ad3824650827fc4eedb2c0
Abhay Goswami <abhaygoswami676@gmail.com>
2020-05-06 19:51:26 +0530

    [TEMP]: fix

--- src/services/sendMail.js
+++ src/services/sendMail.js
@@ -1,19 +1,244 @@
-const sgMail = require("@sendgrid/mail");
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const nodemailer = require("nodemailer");
 
 let template = null;
+let plainText = null
 
-module.exports = function(params) {
+module.exports = async function(params) {
+  console.log(params)
   if (params.type === "WelcomeMail") {
     template = `
-        <h1 style="text-align: center"><span style="font-family: courier new,courier,monospace">Welcome to </span><span style="font-family: courier new,courier,monospace">BHAHI-KHATA🚀</span></h1>
-        <h3 style=""><span style="font-family: courier new,courier,monospace">Thanks for choosing Bhahi-Khata as your expense manager🙏.</span></h3>
-        <h3 style=""><span style="font-family: courier new,courier,monospace">We are committed to making your experience smooth and more enjoyable😍.&nbsp;</span></h3>
-        <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:100% !important; width:100%; height:auto !important;" width="600" alt="" data-proportionally-constrained="true" data-responsive="true" src="http://cdn.mcauto-images-production.sendgrid.net/8b59c3d0b56e8458/8f175730-78eb-4865-9980-c88aec56ce3c/1920x1276.jpg">
-        <h5 style="">BHAH-KHATA is created by <a href="https://abgoswami.netlify.com"><em>Abhay Goswami😎</em></a><em>.</em></h5>
-        <h5 style="">A <u><em>MADE IN INDIA 💓</em></u> product @2020</h5>
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    <html data-editor-version="2" class="sg-campaigns" xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+          <!--[if !mso]><!-->
+          <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+          <!--<![endif]-->
+          <!--[if (gte mso 9)|(IE)]>
+          <xml>
+            <o:OfficeDocumentSettings>
+              <o:AllowPNG/>
+              <o:PixelsPerInch>96</o:PixelsPerInch>
+            </o:OfficeDocumentSettings>
+          </xml>
+          <![endif]-->
+          <!--[if (gte mso 9)|(IE)]>
+      <style type="text/css">
+        body {width: 600px;margin: 0 auto;}
+        table {border-collapse: collapse;}
+        table, td {mso-table-lspace: 0pt;mso-table-rspace: 0pt;}
+        img {-ms-interpolation-mode: bicubic;}
+      </style>
+    <![endif]-->
+          <style type="text/css">
+        body, p, div {
+          font-family: arial,helvetica,sans-serif;
+          font-size: 14px;
+        }
+        body {
+          color: #000000;
+        }
+        body a {
+          color: #1188E6;
+          text-decoration: none;
+        }
+        p { margin: 0; padding: 0; }
+        table.wrapper {
+          width:100% !important;
+          table-layout: fixed;
+          -webkit-font-smoothing: antialiased;
+          -webkit-text-size-adjust: 100%;
+          -moz-text-size-adjust: 100%;
+          -ms-text-size-adjust: 100%;
+        }
+        img.max-width {
+          max-width: 100% !important;
+        }
+        .column.of-2 {
+          width: 50%;
+        }
+        .column.of-3 {
+          width: 33.333%;
+        }
+        .column.of-4 {
+          width: 25%;
+        }
+        @media screen and (max-width:480px) {
+          .preheader .rightColumnContent,
+          .footer .rightColumnContent {
+            text-align: left !important;
+          }
+          .preheader .rightColumnContent div,
+          .preheader .rightColumnContent span,
+          .footer .rightColumnContent div,
+          .footer .rightColumnContent span {
+            text-align: left !important;
+          }
+          .preheader .rightColumnContent,
+          .preheader .leftColumnContent {
+            font-size: 80% !important;
+            padding: 5px 0;
+          }
+          table.wrapper-mobile {
+            width: 100% !important;
+            table-layout: fixed;
+          }
+          img.max-width {
+            height: auto !important;
+            max-width: 100% !important;
+          }
+          a.bulletproof-button {
+            display: block !important;
+            width: auto !important;
+            font-size: 80%;
+            padding-left: 0 !important;
+            padding-right: 0 !important;
+          }
+          .columns {
+            width: 100% !important;
+          }
+          .column {
+            display: block !important;
+            width: 100% !important;
+            padding-left: 0 !important;
+            padding-right: 0 !important;
+            margin-left: 0 !important;
+            margin-right: 0 !important;
+          }
+          .social-icon-column {
+            display: inline-block !important;
+          }
+        }
+      </style>
+          <!--user entered Head Start--><!--End Head user entered-->
+        </head>
+        <body>
+          <center class="wrapper" data-link-color="#1188E6" data-body-style="font-size:14px; font-family:arial,helvetica,sans-serif; color:#000000; background-color:#FFFFFF;">
+            <div class="webkit">
+              <table cellpadding="0" cellspacing="0" border="0" width="100%" class="wrapper" bgcolor="#FFFFFF">
+                <tr>
+                  <td valign="top" bgcolor="#FFFFFF" width="100%">
+                    <table width="100%" role="content-container" class="outer" align="center" cellpadding="0" cellspacing="0" border="0">
+                      <tr>
+                        <td width="100%">
+                          <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                            <tr>
+                              <td>
+                                <!--[if mso]>
+        <center>
+        <table><tr><td width="600">
+      <![endif]-->
+                                        <table width="100%" cellpadding="0" cellspacing="0" border="0" style="width:100%; max-width:600px;" align="center">
+                                          <tr>
+                                            <td role="modules-container" style="padding:0px 0px 0px 0px; color:#000000; text-align:left;" bgcolor="#FFFFFF" width="100%" align="left"><table class="module preheader preheader-hide" role="module" data-type="preheader" border="0" cellpadding="0" cellspacing="0" width="100%" style="display: none !important; mso-hide: all; visibility: hidden; opacity: 0; color: transparent; height: 0; width: 0;">
+        <tr>
+          <td role="module-content">
+            <p></p>
+          </td>
+        </tr>
+      </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="df5abceb-c159-4d17-b72b-71ac6a3ed8d5">
+        <tbody>
+          <tr>
+            <td style="padding:18px 0px 18px 0px; line-height:40px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><h1 style="text-align: center"><span style="font-family: helvetica,sans-serif">Welcome to </span><span style="font-family: helvetica,sans-serif; color: #F9A826">Bhahi-Khata</span></h1><div></div></div></td>
+          </tr>
+        </tbody>
+      </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="2ee11e40-d415-424d-8a64-eba1d911031f">
+        <tbody>
+          <tr>
+            <td style="padding:18px 0px 18px 0px; line-height:30px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><h2 style=""><span style="color: #F9A826; font-family: helvetica,sans-serif">Designed for you to help you in your difficult times. Managing all about expenses taken care of by us from right now.</span></h2><div></div></div></td>
+          </tr>
+        </tbody>
+      </table><table class="wrapper" role="module" data-type="image" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="ef01e70d-eb00-4e04-bfae-b449f3b2dd5a">
+        <tbody>
+          <tr>
+            <td style="font-size:6px; line-height:10px; padding:0px 0px 0px 0px;" valign="top" align="center">
+              <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:100% !important; width:100%; height:auto !important;" width="600" alt="" data-proportionally-constrained="true" data-responsive="true" src="http://cdn.mcauto-images-production.sendgrid.net/8b59c3d0b56e8458/f870c8cc-575e-4056-aac7-382a00212b5e/1108x891.png">
+            </td>
+          </tr>
+        </tbody>
+      </table><table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" role="module" data-type="columns" style="padding:0px 0px 0px 0px;" bgcolor="#FFFFFF">
+        <tbody>
+          <tr role="module-content">
+            <td height="100%" valign="top"><table width="290" style="width:290px; border-spacing:0; border-collapse:collapse; margin:0px 10px 0px 0px;" cellpadding="0" cellspacing="0" align="left" border="0" bgcolor="" class="column column-0">
+          <tbody>
+            <tr>
+              <td style="padding:0px;margin:0px;border-spacing:0;"><table class="wrapper" role="module" data-type="image" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="6b8ad0e3-675b-4048-8681-f2f85ea98c84">
+        <tbody>
+          <tr>
+            <td style="font-size:6px; line-height:10px; padding:0px 0px 0px 0px;" valign="top" align="center">
+              <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:100% !important; width:100%; height:auto !important;" width="290" alt="" data-proportionally-constrained="true" data-responsive="true" src="http://cdn.mcauto-images-production.sendgrid.net/8b59c3d0b56e8458/1bfdee55-3b33-4961-875a-e6edfb27089d/1000x1000.png">
+            </td>
+          </tr>
+        </tbody>
+      </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="7e4c6b46-8590-4bd5-b7f4-7b33bbeffc86">
+        <tbody>
+          <tr>
+            <td style="padding:18px 0px 18px 0px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><div style="font-family: inherit"><span style="color: #F9A826">From now, onwards you can manage your all expenses by using </span><span style="color: #F9A826"><strong>Bhahi-khata ( BK ).</strong></span><span style="color: #F9A826">&nbsp;</span></div><div></div></div></td>
+          </tr>
+        </tbody>
+      </table></td>
+            </tr>
+          </tbody>
+        </table><table width="290" style="width:290px; border-spacing:0; border-collapse:collapse; margin:0px 0px 0px 10px;" cellpadding="0" cellspacing="0" align="left" border="0" bgcolor="" class="column column-1">
+          <tbody>
+            <tr>
+              <td style="padding:0px;margin:0px;border-spacing:0;"><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="5057ca6a-0e17-4e95-8500-d6ca8eee66c8">
+        <tbody>
+          <tr>
+            <td style="padding:18px 0px 18px 0px; line-height:30px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><h2 style=""><span style="color: #F9A826; font-family: helvetica,sans-serif">We are your expenses manager. Hope you will your life time partner within us.</span></h2><div></div></div></td>
+          </tr>
+        </tbody>
+      </table><table class="wrapper" role="module" data-type="image" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="a7092fe7-e76f-4de5-8e38-00dbe98aaf75">
+        <tbody>
+          <tr>
+            <td style="font-size:6px; line-height:10px; padding:0px 0px 0px 0px;" valign="top" align="center">
+              <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:100% !important; width:100%; height:auto !important;" width="290" alt="" data-proportionally-constrained="true" data-responsive="true" src="http://cdn.mcauto-images-production.sendgrid.net/8b59c3d0b56e8458/6e0f0ccf-85de-4a0f-9b32-97d101067828/1000x1000.png">
+            </td>
+          </tr>
+        </tbody>
+      </table></td>
+            </tr>
+          </tbody>
+        </table></td>
+          </tr>
+        </tbody>
+      </table></td>
+                                          </tr>
+                                        </table>
+                                        <!--[if mso]>
+                                      </td>
+                                    </tr>
+                                  </table>
+                                </center>
+                                <![endif]-->
+                              </td>
+                            </tr>
+                          </table>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </center>
+        </body>
+      </html>
         `;
+    plainText = `**********************
+      Welcome to Bhahi-Khata
+      **********************
+      
+      ---------------------------------------------------------------------------------------------------------------------
+      Designed for you to help you in your difficult times. Managing all about expenses taken care of by us from right now.
+      ---------------------------------------------------------------------------------------------------------------------
+      
+      From now, onwards you can manage your all expenses by using *Bhahi-khata ( BK ).*
+      
+      -----------------------------------------------------------------------------
+      We are your expenses manager. Hope you will your life time partner within us.
+      -----------------------------------------------------------------------------`
   } else {
     template = `
     <h1 style="text-align: center"><span style="color: rgba(0, 0, 0, 0.87); font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-style: initial; text-decoration-color: initial; float: none; display: inline; font-family: courier, monospace; font-size: 40px">See You Again⏰</span></h1>
@@ -24,12 +249,30 @@ module.exports = function(params) {
 <h5 style="">A <u><em>MADE IN INDIA</em></u> product @2020</h5>
     `;
   }
-  const msg = {
-    to: params.to,
-    from: params.from,
-    subject: params.subject,
-    text: params.text,
-    html: template
-  };
-  sgMail.send(msg);
+
+  const transporter = nodemailer.createTransport({
+    host: "Gmail",
+    port: 587,
+    auth: {
+      user: `abhaygoswami676@gmail.com`,
+      pass: `Abhay123#Goswami`
+    }
+  });
+
+  const info = await transporter.sendMail({
+    from: params.from, // sender address
+    to: params.to, // list of receivers
+    subject: params.subject, // Subject line
+    text: plainText, // plain text body
+    html: template // html body
+  });
+
+  // const msg = {
+  //   to: params.to,
+  //   from: params.from,
+  //   subject: params.subject,
+  //   text: params.text,
+  //   html: template
+  // };
+  console.log("Message sent: %s", info.messageId);
 };
