4d049cc8e68b1e91293e3748271efd29df5dba53
Abhay Goswami <abhaygoswami676@gmail.com>
2020-05-06 19:49:44 +0530

    remove params field

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -5,9 +5,9 @@ const auth = require("../api/middleware/Auth")
 
 router.post("/login", loginController.login);
 router.post("/register", loginController.register);
-router.patch("/user/update/:id", auth, loginController.update);
-router.delete("/user/delete/:id", auth, loginController.deleteAcc);
-router.post("/user/2fa/:id", auth, loginController.twoFA);
-router.get("/user/:id", auth, loginController.getUser);
+router.patch("/user/update", auth, loginController.update);
+router.delete("/user/delete", auth, loginController.deleteAcc);
+router.post("/user/2fa", auth, loginController.twoFA);
+router.get("/user", auth, loginController.getUser);
 
 module.exports = router;
