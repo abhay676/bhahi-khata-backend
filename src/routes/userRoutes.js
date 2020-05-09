32151cf885f6f0d29949db504b3108aa5f1712e8
Abhay Goswami <abhaygoswami676@gmail.com>
2020-05-10 00:56:05 +0530

    Rename loginRoutes to userRoutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,12 @@
-const router = require("express").Router()
+const router = require("express").Router();
 
 const loginController = require("../api/controller/UserController");
-const auth = require("../api/middleware/Auth")
-
+const auth = require("../api/middleware/Auth");
 router.post("/login", loginController.login);
 router.post("/register", loginController.register);
 router.patch("/user/update", auth, loginController.update);
 router.delete("/user/delete", auth, loginController.deleteAcc);
-router.post("/user/2fa", auth, loginController.twoFA);
+// router.post("/user/2fa", auth, loginController.twoFA);
 router.get("/user", auth, loginController.getUser);
 
 module.exports = router;
