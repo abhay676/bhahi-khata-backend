c90178c33392917e9f634a5b5252c4324709da5e
Abhay Goswami <abhaygoswami676@gmail.com>
2020-05-11 02:01:27 +0530

    update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,19 @@
 const express = require("express")
 
-const loginRoutes = require("./loginRoutes")
+const userRoutes = require("./userRoutes")
 const walletRoutes = require("./walletRoutes")
-const expensesRoutes = require("./walletRoutes")
+const expensesRoutes = require("./expensesRoutes")
 const filterRoutes = require("./filterRoutes")
 const reportRoutes = require("./reportRoutes")
+const categoryRoutes = require("./categoryRoutes")
 
 const router = express.Router()
 
-router.use(loginRoutes)
+router.use(userRoutes)
 router.use(walletRoutes)
 router.use(expensesRoutes)
 router.use(filterRoutes)
 router.use(reportRoutes)
+router.use(categoryRoutes)
 
 module.exports = router;
--- src/routes/walletRoutes.js
+++ src/routes/walletRoutes.js
@@ -3,14 +3,10 @@ const router = require("express").Router();
 const walletsController = require("../api/controller/WalletsController");
 const auth = require("../api/middleware/Auth")
 
-router.post("/api/wallets/add", auth, walletsController.add);
-router.patch("/api/wallets/update/:id", auth, walletsController.update);
-router.delete("/api/wallets/delete/:id", auth, walletsController.delete);
-router.get("/api/wallet/:id", auth, walletsController.getWallet);
-router.get("/api/wallets", auth, walletsController.allWallets);
-// ? Fetch All expenses for a particular Wallet
-router.get("/api/expenses/:walletId", auth, walletsController.walletExpenses);
-// Freeze wallet
-router.get("/api/freezeWallet/:walletId", auth, walletsController.freezeWallet);
+router.post("/wallet/add", auth, walletsController.add);
+router.patch("/wallet/update", auth, walletsController.update);
+router.delete("/wallet/delete", auth, walletsController.delete);
+router.get("/wallet", auth, walletsController.getWallet);
+router.get("/wallet/freeze", auth, walletsController.freezeWallet);
 
 module.exports = router;
