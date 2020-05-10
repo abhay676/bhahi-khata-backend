f22fa9236b2b67d253f998d1efbc7af9500dea30
Abhay Goswami <abhaygoswami676@gmail.com>
2020-05-11 02:01:03 +0530

    minor-fx: change timestamp format

--- src/services/Winston.js
+++ src/services/Winston.js
@@ -1,8 +1,9 @@
 const { createLogger, format, transports } = require("winston");
+const moment = require("moment")
 const { combine, timestamp, label, printf } = format;
 
 const myFormat = printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} [${label}] ${level}: ${message}`;
+  return `${moment(timestamp)} [${label}] ${level}: ${message}`;
 });
 
 const logger = createLogger({
