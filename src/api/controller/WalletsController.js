773cd1593d40e2b684fba94901f93f99e1489374
Abhay Goswami <abhaygoswami676@gmail.com>
2020-05-11 13:33:12 +0530

    update wallet controller

--- src/api/controller/WalletsController.js
+++ src/api/controller/WalletsController.js
@@ -1,42 +1,23 @@
 const Wallets = require("../model/Wallets");
-const User = require("../model/User");
 const { ErrorHandler, Responsehandler } = require("../../services/Handler");
 
 exports.add = async (req, res, next) => {
-  const id = req.user;
   try {
     const wallet = new Wallets({ ...req.body, user: req.user });
-    wallet.save().then((data) => {
-      res.send(Responsehandler(data));
-      return User.findByIdAndUpdate(id, {
-        $push: { wallets: data }
-      });
-    });
+    await wallet.save();
+    res.send(Responsehandler(wallet.walletInfo()));
   } catch (error) {
     error.code = 403;
     next(error);
   }
 };
 
-// Not a good approach
 exports.update = async (req, res, next) => {
   const id = req.query.id;
   try {
     const wallet = await Wallets.findOneAndUpdate(
       { walletId: id },
       { ...req.body },
-      { new: true }
-    );
-    // First remove the object
-    await User.findByIdAndUpdate(
-      req.user,
-      { $pull: { wallets: { walletId: id } } },
-      { new: true }
-    );
-    // Added the updated object
-    await User.findByIdAndUpdate(
-      req.user,
-      { $push: { wallets: wallet } },
       { new: true }
     );
     res.send(Responsehandler(wallet.walletInfo()));
@@ -50,36 +31,12 @@ exports.delete = async (req, res, next) => {
   const id = req.query.id;
   try {
     await Wallets.find({ walletId: id });
-    await User.findByIdAndUpdate(
-      req.user,
-      { $pull: { wallets: { walletId: id } } },
-      { new: true }
-    );
     res.send(Responsehandler("Deleted"));
   } catch (error) {
     error.code = 500;
     next(error);
   }
 };
-
-// exports.walletExpenses = async (req, res) => {
-//   try {
-//     const walletId = req.params.walletId;
-//     const expenses = await Wallets.findById(walletId);
-//     if (!expenses) throw new Error(msg.expenseAllError);
-//     if (expenses.freeze) {
-//         generateMsg(msg.freezeWalletExpense, "error", msg.freezeWalletExpense)
-//       return res.send(
-//       );
-//     }
-//     await expenses.populate("transactions").execPopulate();
-//     res.send(
-//       generateMsg(msg.expenseAllSuccess, "success", expenses.transactions)
-//     );
-//   } catch (error) {
-//     res.send(generateMsg(null, "error", error));
-//   }
-// };
 
 exports.getWallet = async (req, res, next) => {
   try {
@@ -100,7 +57,7 @@ exports.freezeWallet = async (req, res, next) => {
     if (walletInfo.isFreezed) {
       return res.send(Responsehandler("Wallet is already freezed"));
     }
-    const wallet = await Wallets.updateOne(
+    await Wallets.updateOne(
       { walletId: id },
       {
         $set: {
@@ -108,14 +65,19 @@ exports.freezeWallet = async (req, res, next) => {
         }
       }
     );
-    await User.updateOne(
-      { _id: req.user, "wallets.walletId": id },
-      { $set: { "wallets.$.isFreezed": true } }
-    );
-    if (!wallet) throw new ErrorHandler(404, "No wallet found");
     res.send(Responsehandler("Wallet freezed successfully"));
   } catch (error) {
     error.code = 500;
+    next(error);
+  }
+};
+
+exports.getAllWallets = async (req, res, next) => {
+  try {
+    const wallet = await Wallets.find({ user: req.user });
+    res.send(Responsehandler(wallet));
+  } catch (error) {
+    error.code = 404;
     next(error);
   }
 };
